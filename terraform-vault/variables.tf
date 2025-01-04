66ca94d55678567e0694ba24b87e0b7708b17b94
Sachin20010517 <sachinayeshmantha@gmail.com>
2025-01-04 18:32:28 +0530

    first commit

---
+++ terraform-vault/variables.tf
@@ -0,0 +1,13 @@
+variable "vault_addr" {
+  default = "https://kmsvault.sachinayeshmantha.live:8200"
+}
+variable "vault_token" {
+  default = "TOKEN-HERE"
+}
+
+variable "vm_count" {
+  default = 3
+}
+
+
+
